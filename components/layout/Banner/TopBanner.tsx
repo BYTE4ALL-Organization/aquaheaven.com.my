6927c528353ca5cd28f742a9a1b7c748817504ff
byte4all <190314524+byte4all@users.noreply.github.com>
2026-09-07 11:36:52 +0800

    Changed shipping to 60 RM

--- components/layout/Banner/TopBanner.tsx
+++ components/layout/Banner/TopBanner.tsx
@@ -5,7 +5,7 @@ const TopBanner = () => {
     <div className="bg-black text-white text-center py-2 px-2 sm:px-4 xl:px-0">
       <div className="max-w-frame mx-auto">
         <p className="text-xs sm:text-sm">
-          Free Shipping From 35 RM Spend Across Malaysia. & 5% OFF On All Kiwi
+          Free Shipping From 60 RM Spend Across Malaysia. & 5% OFF On All Kiwi
           Towels & Foutas !
         </p>
       </div>
